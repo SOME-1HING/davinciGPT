595ea3fc3a3ee80ccb6dd8c2d036fa1df4ad7de2
SOME1HING <yashprakash2005@gmail.com>
2023-01-04 02:13:46 +0530

    fixed

--- davinciGPT/main.py
+++ davinciGPT/main.py
@@ -1,17 +1,19 @@
-from requests import get
+from requests import post
+import json
 
 
 class Chatbot:
     def __init__(self, token):
-        self.url = "https://codex-sfr4.onrender.com"
+        self.url = "https://codex-sfr4.onrender.com/"
         self.token = token
 
     def chatbot(self, query):
-        query = "+".join(query.split(" "))
         try:
-            url = f"{self.url}/{query}"
-            res = str(get(url).json()["bot"])
+            body = {"prompt": query}
+            res = post(self.url, json=body)
+            res = str(res.json()["bot"])
             result = " ".join(res.split())
+
             return result
         except:
             return "ERROR!! Chatbot not responding."
